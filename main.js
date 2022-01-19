7d2cdad19e32356f4a52ee444a686602041528ff
Amey Sawant <amey2804@gmail.com>
2022-01-19 16:38:21 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const translateInputText = document.getElementById("userTextInput")
+const translateBtnEl = document.querySelector("#btn-translate")
+const translateOutputDivEl = document.querySelector("#outputDiv")
+const serviceURL = "https://api.funtranslations.com/translate/minion.json"
+// const serviceURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+function constructURL(text) {
+    let encodedURI = encodeURI(text);
+    console.log(`${serviceURL}?text=${encodedURI}`)
+    return `${serviceURL}?text=${encodedURI}`
+}
+translateBtnEl.addEventListener("click", () => {
+    let input = translateInputText.value
+    if(input == "") {
+        alert("You have not entered the text to be translated.. Kindly enter the text to be translated")
+        return
+    }
+    let finalURL = constructURL(input)
+    fetch(finalURL)
+    .then(res => res.json())
+    .then(json => translateOutputDivEl.textContent = json.contents.translated)
+    .catch(() => {
+        alert("We are facing some issue right now.. Please try again later")
+    })
+})
